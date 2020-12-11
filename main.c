cd0e9daac71bc7f3c08af8cc76a253849d527b01
Tymoteusz Jeske-Stock <75068968+Swiezacek@users.noreply.github.com>
2020-12-11 11:48:40 +0100

    Kod Kalkulator

--- main.c
+++ main.c
@@ -2,20 +2,35 @@
 
 int main ()
 {
-    float a, b, d;
+    int a, b;
+    float d;
     char c;
 
     printf("Podaj dzialanie jakie chcesz wykonac: \n");
-    scanf("%f %c %f", &a, &c, &b);
+    scanf("%d %c %d", &a, &c, &b);
 
     switch (c)
     {
-        case   '+': d=a+b;break;
-        case   '-': d=a-b;break;
-        case   '*': d=a*b;break;
-        case   '/': d=a/b;
-            if (b==0)
-                printf ("Nie dzieli sie przez zero!");return 0;
+        case '+':
+            d=a+b;
+            break;
+        case '-':
+            d=a-b;
+            break;
+        case '*':
+            d=a*b;
+            break;
+        case '/':
+            if (b==0) {
+                printf("Nie dzieli sie przez zero!");
+                return 0;
+            }
+            d=(float)a/b;
+            break;
+
+        default:
+            printf("blad w dzialaniu");
+            return 0;
     }
     printf("Wynik to: %.2f\n", d);
     return 0;
