0e55344f3bb261ddea362003414334ea6a136751
Tymoteusz Jeske-Stock <75068968+Swiezacek@users.noreply.github.com>
2020-12-11 11:08:36 +0100

    Kod Kalkulator

--- main.c
+++ main.c
@@ -5,32 +5,18 @@ int main ()
     int a, b, d;
     char c;
 
-    printf("Podaj pierwsza liczbe : \n");
-    scanf("%d", &a);
-
-    printf("Podaj druga liczbe: \n");
-    scanf("%d", &b);
-
-    getchar();
-    printf("Co chcesz zrobic (podaj znak): \n");
-    scanf("%c", &c);
+    printf("Podaj dzialanie jakie chcesz wykonac: \n");
+    scanf("%d %c %d", &a, &c, &b);
 
     switch (c)
     {
-        case   '+': d=a+b;
-        printf("Wynik to: %d\n", d);break;
-
-        case   '-': d=a-b;
-            printf("Wynik to: %d\n", d);break;
-
-        case   '*': d=a*b;
-        printf("Wynik to: %d\n", d);break;
-
+        case   '+': d=a+b;break;
+        case   '-': d=a-b;break;
+        case   '*': d=a*b;break;
         case   '/': d=a/b;
-            printf("Wynik to: %d\n", d);
-
             if (b==0)
                 printf ("Nie dzieli sie przez zero ");break;
     }
+    printf("Wynik to: %d\n", d);
     return 0;
 }
